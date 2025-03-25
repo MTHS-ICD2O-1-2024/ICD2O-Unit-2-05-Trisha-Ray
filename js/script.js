7ef79e861e2616b6e21085a97eda49c478253163
default <trisha.ray@stu.ocsb.ca>
2025-03-25 16:45:24 +0000

    js fixed

--- js/script.js
+++ js/script.js
@@ -6,16 +6,15 @@
 
 "use strict"
 
-function calculatesalary () {
+function calculateSalary () {
   // input
   const hoursworked = parseFloat(document.getElementById('hours-worked').value)
   const hourlywage = parseFloat(document.getElementById('hourly-wage').value)
 
   // process
-  const weeklypay = (hoursworked * hourlywage) * (1.00 * 0.18)
-  const incometax = (hoursworked * hourlywage) * 0.18
-
+  const weeklypay = hoursworked * hourlywage * (1 - 0.18)  
+  const incometax = hoursworked * hourlywage * 0.18 
   // output
-  document.getElementById('weeklpay').innerHTML = `Your pay will be: $ ${weeklypay.toFixed(2)}`
+  document.getElementById('weeklypay').innerHTML = 'Your pay will be: $' +  weeklypay.toFixed(2)
   document.getElementById('incometax').innerHTML = 'The government will take: $' + incometax.toFixed(2)
-  }
+}
